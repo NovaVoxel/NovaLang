16d7848afdd12e09615d8636ea298dd2a1894846
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-29 22:49:20 +0100

    Update ast.py

--- compiler/ast.py
+++ compiler/ast.py
@@ -1,23 +1,69 @@
-class FunctionDef:
-    """
-    AST node representing a function definition in Nova.
+# compiler/ast.py
 
-    Example:
-        def add(x, y):
-            return x + y
-    """
 
-    def __init__(self, name, params=None, body=None, return_type=None, location=None):
+class BlockNode:
+    def __init__(self, statements, span=None):
+        self.statements = statements
+        self.span = span
+
+
+class WhileNode:
+    def __init__(self, condition, body, span=None):
+        self.condition = condition
+        self.body = body
+        self.span = span
+
+
+class ForEachNode:
+    """
+    for i expr { ... }
+    for k,v expr { ... }
+    vars: ["i"] oder ["k","v"]
+    iterable: ExpressionNode
+    body: BlockNode
+    """
+    def __init__(self, vars_, iterable, body, span=None):
+        self.vars = vars_
+        self.iterable = iterable
+        self.body = body
+        self.span = span
+
+
+class VarAssignNode:
+    def __init__(self, name, expr, span=None):
         self.name = name
-        self.params = params or []      # list of parameter names
-        self.body = body                # Block node
-        self.return_type = return_type  # optional type annotation
-        self.location = location        # (line, column) for error reporting
+        self.expr = expr
+        self.span = span
 
-    def __repr__(self):
-        return (
-            f"FunctionDef(name={self.name!r}, "
-            f"params={self.params}, "
-            f"return_type={self.return_type}, "
-            f"body={self.body})"
-        )
+
+class VarRefNode:
+    def __init__(self, name, span=None):
+        self.name = name
+        self.span = span
+
+
+class CallNode:
+    def __init__(self, func, args, span=None):
+        self.func = func
+        self.args = args
+        self.span = span
+
+
+class NumberNode:
+    def __init__(self, value, span=None):
+        self.value = value
+        self.span = span
+
+
+class StringNode:
+    def __init__(self, value, span=None):
+        self.value = value
+        self.span = span
+
+
+class BinaryOpNode:
+    def __init__(self, left, op, right, span=None):
+        self.left = left
+        self.op = op
+        self.right = right
+        self.span = span
