a44397a152bb2056e5f8663cb27180b5a01f7425
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-29 01:54:31 +0100

    Update parser.py

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,4 +1,11 @@
-from .ast import FunctionDef
+class FunctionDef:
+    def __init__(self, name, body):
+        self.name = name
+        self.body = body
+
+class UseStmt:
+    def __init__(self, module):
+        self.module = module
 
 def parse(tokens):
     ast = []
@@ -8,6 +15,10 @@ def parse(tokens):
         if tokens[i][0] == "DEF":
             name = tokens[i+1][1]
             ast.append(FunctionDef(name, []))
+            i += 2
+        elif tokens[i][0] == "USE":
+            module = tokens[i+1][1]
+            ast.append(UseStmt(module))
             i += 2
         else:
             i += 1
